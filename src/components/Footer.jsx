ba3597be346664f0de3a6f65cd765fab62c4eb22
system.conf <73934647+system-conf@users.noreply.github.com>
2024-08-07 18:59:09 +0000

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-primary text-secondary p-4 text-center mt-8">
-      <p>Created by <a href="https://github.com/system-conf" > <b>system.conf</b> </a>    / <a href="https://github.com/system-conf" className="text-accent">GitHub</a> / License</p>
+      <p>Created by <a href="https://github.com/system-conf" > <b>system.conf</b> </a> / <a href="https://github.com/system-conf/boykiloindeks">GitHub</a> / License</p>
     </footer>
   );
 };
