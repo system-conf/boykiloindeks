7ccee0441919592153254416a2b863fa93044674
system.conf <73934647+system-conf@users.noreply.github.com>
2024-08-08 12:52:39 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { Helmet } from 'react-helmet';
 import Header from './components/Header';
 import BMICalculator from './components/BMICalculator';
 import Footer from './components/Footer';
@@ -14,6 +15,12 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-white flex flex-col justify-between">
+      <Helmet>
+        <title>Vücut Kitle Endeksi Hesaplama</title>
+        <meta name="description" content="Vücut Kitle Endeksi (BMI) hesaplama aracı ile sağlığınızı takip edin." />
+        <meta name="keywords" content="BMI, Vücut Kitle Endeksi, Sağlık, Kilo, Boy" />
+        <meta name="author" content="Süleyman Talha Duman" />
+      </Helmet>
       <Header onReset={handleReset} className="bg-black text-white" />
       <main className="p-4 flex-grow bg-white text-black">
         <BMICalculator ref={bmiCalculatorRef} />
